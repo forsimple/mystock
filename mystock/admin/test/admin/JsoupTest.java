f076ce8bbaf0e70b9a102fc96992889dbc203c3b
forsimple <whwycx@gmail.com>
2016-08-26 18:39:48 +0800

    url常量引入

--- mystock/admin/test/admin/JsoupTest.java
+++ mystock/admin/test/admin/JsoupTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import me.wh.stock.admin.Const.TicketConstant;
 import me.wh.stock.admin.util.JsoupUtils;
 
 import org.jsoup.Jsoup;
@@ -15,6 +16,8 @@ import org.jsoup.select.Elements;
 import org.junit.Test;
 
 public class JsoupTest {
+    
+    
     
     @Test
     public void parseTableTest(){
@@ -41,6 +44,7 @@ public class JsoupTest {
     }
     @Test
     public void getTableTest(){
+        System.out.println(String.format(TicketConstant.SINA_DAY_PRICE_URL, "http://",TicketConstant.domainMap.get("vsf"),TicketConstant.pageMap.get("jv"),1));
         String urlFormat="http://vip.stock.finance.sina.com.cn/corp/go.php/vMS_FuQuanMarketHistory/stockid/%s.phtml?year=%d&jidu=%d";
         JsoupUtils.getTableById(String.format(urlFormat, "000002",2016,3), "FundHoldSharesTable", 1);
     }
